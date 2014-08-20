17e911b840bdadd38531c8dfb460cf4ab7bb5496
HsienWei Hsiang <george.hsiang@udngroup.com.tw>
2014-08-20 17:35:56 +0800

    測試名稱功能

--- app.js
+++ app.js
@@ -2,20 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+var socket_map = {};
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  
+  console.log(socket.id);
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    delete socket_map[socket.id];
   });
+
   socket.on('chatmessage', function(msg){
     console.log('message: ' + msg);
-    io.emit('chatmessage', msg);
+    if(socket_map[socket.id] != null)
+      io.emit('chatmessage', socket_map[socket.id] +':' +msg);
+    else
+      io.emit('chatmessage', 'noname' +':' +msg);
   });
+
+  socket.on('set_name', function(name){
+    console.log('set_name: ' + name);
+    socket_map[socket.id] = name;
+    
+  });
+
 
 });
 
